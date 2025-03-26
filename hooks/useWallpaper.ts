1efbf2c418b4b340755b39ad3dd155c890eb2edc
Chirag Kushwaha <chirag.kushwaha@eka.care>
2025-03-27 05:27:14 +0530

    Animated wallpaper added

--- hooks/useWallpaper.ts
+++ hooks/useWallpaper.ts
@@ -3,18 +3,26 @@ import WAVES from '@/public/libs/vanta.waves.min';
 import { useEffect } from 'react';
 import * as THREE from 'three';
 
+const disableControls = {
+  mouseControls: true,
+  touchControls: true
+};
+const settings = {
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0
+};
+
 const useWallpaper = (desktopRef: React.RefObject<HTMLElement | null>) => {
   useEffect(() => {
+    //@ts-expect-error WAVES does not have types//
     const vantaEffect = WAVES({
       el: desktopRef.current,
       THREE,
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 1.0,
-      scaleMobile: 1.0
+      ...disableControls,
+      ...settings
     });
 
     return () => {
