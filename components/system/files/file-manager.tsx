6c119af57b254baa9c86287e05e91e3644819ad4
Chirag Kushwaha <chirag.kushwaha@eka.care>
2025-04-01 01:42:34 +0530

    styled file manager grid

--- components/system/files/file-manager.tsx
+++ components/system/files/file-manager.tsx
@@ -10,15 +10,28 @@ type FileManagerProps = {
 
 const FileManager = ({ directory }: FileManagerProps) => {
   return (
-    <ol className="max-w-min">
-      {useFiles(directory, (file) => (
-        <FileEntry
-          key={file}
-          name={basename(file, extname(file))}
-          path={resolve(directory, file)}
-        />
-      ))}
-    </ol>
+    <>
+      <ol className="w-full h-full file-manager">
+        {useFiles(directory, (file) => (
+          <FileEntry
+            key={file}
+            name={basename(file, extname(file))}
+            path={resolve(directory, file)}
+          />
+        ))}
+      </ol>
+      <style jsx>{`
+        .file-manager {
+          display: grid;
+          grid-auto-flow: column;
+          grid-template-columns: repeat(auto-fill, 74px);
+          grid-template-rows: repeat(auto-fill, 70px);
+          padding: 5px, 0;
+          row-gap: 28px;
+          column-gap: 1px;
+        }
+      `}</style>
+    </>
   );
 };
 
