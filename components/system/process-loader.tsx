ccf3f82757b2d1dfba56bc9539dd3f804eae80db
Chirag Kushwaha <chirag.kushwaha@eka.care>
2025-03-31 12:38:01 +0530

    test failing due to filesystem need to fix

--- components/system/process-loader.tsx
+++ components/system/process-loader.tsx
@@ -1,10 +1,9 @@
 import useProcessesState from '../../contexts/process';
-import useFileSystem from '../../hooks/useFileSystem';
 import RenderProcess from './render-process';
 
 const ProcessLoader = () => {
   const processes = useProcessesState((state) => state.processes);
-  const {} = useFileSystem();
+  // const {} = useFileSystem();
   return Object.entries(processes).map(([id, process]) => {
     return <RenderProcess key={id} {...process} />;
   });
