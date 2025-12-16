20e49097eac7e1bdce1c01a26af3ac5deaca68de
Chirag Kushwaha <chirag@fileo.tech>
2025-12-17 01:44:38 +0530

    feat: Add `critters` and enable CSS optimization in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  experimental: {
+    optimizeCss: true, // Requires 'critters' package
+  },
 
   images: {
     remotePatterns: [
